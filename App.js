f17c81b3284aa6511e61eff305883fd2f5ad3c56
Emmanuel Yeboah <noelzappy@gmail.com>
2022-03-25 10:21:09 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview";
+import {
+  BackHandler,
+  ActivityIndicator,
+  ImageBackground,
+  SafeAreaView,
+  View,
+} from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import FAB from "react-native-fab";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+const App = () => {
+  const webViewRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleBackButtonPress = () => {
+    try {
+      webViewRef.current?.goBack();
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButtonPress);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}
+    >
+      <WebView
+        source={{ uri: "https://pet.noelzappy.xyz/" }}
+        ref={webViewRef}
+        originWhiteList={["*"]}
+        onLoadStart={() => {
+          setIsLoading(true);
+        }}
+        onLoadEnd={() => {
+          setIsLoading(false);
+        }}
+      />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {isLoading ? (
+        <ImageBackground
+          source={require("./assets/bgsmall.jpg")}
+          resizeMode="cover"
+          style={{
+            flex: 1,
+            position: "absolute",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+            }}
+          >
+            <ActivityIndicator size="large" color="#fff" />
+          </View>
+        </ImageBackground>
+      ) : (
+        <FAB
+          buttonColor="#8A5CF7"
+          iconTextColor="#FFFFFF"
+          onClickAction={() => {
+            webViewRef.current.reload();
+          }}
+          visible={true}
+          iconTextComponent={<Ionicons name="refresh-circle" />}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default App;
